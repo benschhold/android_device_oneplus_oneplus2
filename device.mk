374d22a60321386906ffbdf33844fffd6fb571d5
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-06-14 00:57:44 +0100

    oneplus2: update audio setup
    * remove dolby dap flags and blobs
    * add extra tiny tools
    * remove some build flags
    * change fluence type to fluence and roll back MTP files to fix loadspeaker volume control
    
    Change-Id: I3a70e16f61a25816500aa74b83f84edd6c4d42ff

--- device.mk
+++ device.mk
@@ -104,8 +104,13 @@ PRODUCT_PACKAGES += \
     audio.primary.msm8994 \
     audio.r_submix.default \
     audio.usb.default \
-    audio_policy.msm8994 \
-    tinymix
+    audio_policy.msm8994
+
+PRODUCT_PACKAGES += \
+    tinycap \
+    tinymix \
+    tinypcminfo \
+    tinyplay
 
 PRODUCT_PACKAGES += \
     libaudio-resampler \
