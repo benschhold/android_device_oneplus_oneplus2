24dbc56829581b39078d63556c5967dc0f478c5e
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-06-15 00:02:00 +0100

    oneplus2: update bluetooth setup
    * add missing props
    * remove unused flag
    
    Change-Id: Ia794b1892a7bf47f09d548a13e70ba577bb33d95

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,13 +17,13 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   ""
+#define BTM_DEF_LOCAL_NAME   "OnePlus 2"
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
 #define MAX_ACL_CONNECTIONS    7
 #define MAX_L2CAP_CHANNELS    16
+#define BLE_VND_INCLUDED   TRUE
 // skips conn update at conn completion
 #define BTA_BLE_SKIP_CONN_UPD  FALSE
-#define BLE_VND_INCLUDED   TRUE
 #define BLE_PERIPHERAL_ADV_NAME  FALSE
 #endif
