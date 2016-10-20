25eb477d2d3adc127adb5649f5d206a2ff75bb43
Willi Ye <williye97@gmail.com>
2016-10-20 15:41:04 +0200

    Remove charger images

--- device.mk
+++ device.mk
@@ -112,10 +112,6 @@ PRODUCT_PACKAGES += \
     sensors.hal.tof \
     Snap
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
-
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
     cneapiclient \
