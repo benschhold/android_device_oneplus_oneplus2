b47e5047e1aa58e7c65c520650b8baf536aac838
benschhold <benschhold@gmail.com>
2017-02-25 17:55:15 +0100

    Revert "fix gestures"
    This reverts commit 79e60ff07ccc88dcb7222a92681cd0f4149bb0df.

--- cmhw/org/cyanogenmod/hardware/TouchscreenGesture.java
+++ cmhw/org/cyanogenmod/hardware/TouchscreenGesture.java
@@ -44,17 +44,25 @@ public class TouchscreenGestures {
         "/proc/touchpanel/left_arrow_enable",
         "/proc/touchpanel/right_arrow_enable",
         "/proc/touchpanel/double_swipe_enable",
+        "/proc/touchpanel/up_swipe_enable",
+        "/proc/touchpanel/down_swipe_enable",
+        "/proc/touchpanel/left_swipe_enable",
+        "/proc/touchpanel/right_swipe_enable",
         "/proc/touchpanel/letter_o_enable",
     };
 
     // Id, name, keycode
     private static final TouchscreenGesture[] TOUCHSCREEN_GESTURES = {
-        new TouchscreenGesture(0, "Up arrow", 252),
-        new TouchscreenGesture(1, "Down arrow", 255),
-        new TouchscreenGesture(2, "Left arrow", 254),
-        new TouchscreenGesture(3, "Right arrow", 253),
+        new TouchscreenGesture(0, "Up arrow", 255),
+        new TouchscreenGesture(1, "Down arrow", 252),
+        new TouchscreenGesture(2, "Left arrow", 253),
+        new TouchscreenGesture(3, "Right arrow", 254),
         new TouchscreenGesture(4, "Two finger down swipe", 251),
-        new TouchscreenGesture(5, "Letter O", 250),
+        new TouchscreenGesture(5, "One finger up swipe", 66),
+        new TouchscreenGesture(6, "One finger down swipe", 65),
+        new TouchscreenGesture(7, "One finger left swipe", 64),
+        new TouchscreenGesture(8, "One finger right swipe", 63),
+        new TouchscreenGesture(9, "Letter O", 250),
     };
 
     /**
